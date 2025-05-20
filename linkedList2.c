1ce313f22705a0022b28565cf372c8c1753aace3
Sergey Chukhno <sergey.chukhno@laplateforme.io>
2025-05-20 10:48:42 +0200

    🔥 Linked List: inserting a node at a specific position
    Inserting a node at a specific position implemented

---
+++ linkedList2.c
@@ -0,0 +1,48 @@
+// This program implements a singly linked list in C with functions to inset a new node at a specific position. 
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+struct Node {
+  int data;
+  struct Node *next;
+};
+
+struct Node* head;
+
+void Insert(int data, int n) {
+  struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+  newNode->data = data;
+  newNode->next = NULL; 
+  if (n==1) {
+    newNode->next = head;
+    head = newNode;
+    return;
+  }
+  struct Node *temp = head;
+  for (int i = 0; i < n - 2; i++) {
+    temp = temp->next; 
+  }
+  newNode->next = temp->next;
+  temp->next = newNode; 
+};
+
+void Print() {
+  struct Node *temp = head; 
+  printf("The linked list is: ");
+  while (temp != NULL) {
+    printf(" %d ", temp->data);
+    temp = temp->next;
+  }
+  printf("\n");
+};
+
+int main() {
+  head = NULL; //empty list
+  Insert(2, 1); // insert number 2 at position 1 
+  Insert(3, 2); // insert number 3 at position 2
+  Insert(4, 2); // insert number 4 at position 3
+  Insert(5, 1); // insert number 5 at position 4
+  Print(); 
+}
