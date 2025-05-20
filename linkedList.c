a5e5b39de67b85246a279de40dde9f5a9c68a975
Sergey Chukhno <sergey.chukhno@laplateforme.io>
2025-05-20 10:16:47 +0200

    🚀 Linked List added
    Implemented algorithm to insert nodes at the beginning of the list

--- linkedList.c
+++ linkedList.c
@@ -6,20 +6,43 @@ struct Node {
   struct Node *next;
 };
 
-struct Node* head = NULL;
+struct Node* head;
+
+// Function to insert a new node at the beginning
+// of the linked list
+void Insert(int x) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = x;
+    newNode->next = head;
+    head = newNode;
+}
+    
+
+void Print() {
+  struct Node *temp = head; 
+  printf("The linked list is: ");
+  while (temp != NULL) {
+    printf(" %d ", temp->data);
+    temp = temp->next;
+  }
+  printf("\n");
+};
 
 int main() {
-    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    head = NULL;
+    printf("How many numbers?\n");
+    int n, i, x;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) {
+      printf("Enter number\n"); 
+      scanf("%d", &x);
+      Insert(x);
+      Print(); 
+    }
+      struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
     temp->data = 2;
     temp->next = NULL;
     head = temp;
     return 0;
 }
 
-//Inserting a node at the beginning
-void insertAtBeginning(int newData) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    newNode->data = newData;
-    newNode->next = head;
-    head = newNode;
-}
